8e2c92af1884b21c5e6503b38976e81395dec1a4
Łukasz Głowiński <lukasz.glowinski@o2.pl>
2020-08-09 19:57:29 +0200

    Migrated files

--- BoardGamesManagementTests/Controllers/GamesAPITest.cs
+++ BoardGamesManagementTests/Controllers/GamesAPITest.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using BoardGamesManagement.Controllers;
 using BoardGamesManagement.Database.UnitsOfWork;
@@ -45,7 +43,7 @@ namespace BoardGamesManagementTests.Controllers
             var unitOfWorkMock = new Mock<IUnitOfWork>();
             unitOfWorkMock.Setup(p => p.GamesRepository.GetGamesAsync(It.IsAny<int?>())).ReturnsAsync(gamesList);
 
-            var gamesApiController = new GamesAPI(unitOfWorkMock.Object, new GameHelper(new GameHistoryHelper()));
+            var gamesApiController = new GamesAPI(unitOfWorkMock.Object, new GameHelper(new GameHistoryHelper()), new GameListHelper());
 
             var result = await gamesApiController.GetGames(new GetGamesRequest() { MaxResults = null });
 
@@ -58,9 +56,37 @@ namespace BoardGamesManagementTests.Controllers
             var unitOfWorkMock = new Mock<IUnitOfWork>();
             unitOfWorkMock.Setup(p => p.GamesRepository.GetGamesAsync(It.IsAny<int?>())).ReturnsAsync(new List<Game>());
 
-            var gamesApiController = new GamesAPI(unitOfWorkMock.Object, new GameHelper(new GameHistoryHelper()));
+            var gamesApiController = new GamesAPI(unitOfWorkMock.Object, new GameHelper(new GameHistoryHelper()), new GameListHelper());
 
             var result = await gamesApiController.GetGames(new GetGamesRequest() { MaxResults = null });
+
+            Assert.IsInstanceOf(typeof(NotFoundResult), result.Result);
+        }
+
+        [Test]
+        public async Task GamesAPI_GetGameDetailsById_Returns200OK()
+        {
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            unitOfWorkMock.Setup(p => p.GamesHistoryRepository.AddToVisitHistory(It.IsAny<GameHistory>()));
+            unitOfWorkMock.Setup(p => p.GamesRepository.GetGameByIdAsync(It.IsAny<Guid>())).ReturnsAsync(gamesList[0]);
+
+            var gamesApiController = new GamesAPI(unitOfWorkMock.Object, new GameHelper(new GameHistoryHelper()), new GameListHelper());
+
+            var result = await gamesApiController.GetGameDetailsById(Guid.NewGuid());
+
+            Assert.IsInstanceOf(typeof(OkObjectResult), result.Result);
+        }
+
+        [Test]
+        public async Task GamesAPI_GetGameDetailsById_Returns404NotFound()
+        {
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            unitOfWorkMock.Setup(p => p.GamesRepository.GetGameByIdAsync(It.IsAny<Guid>())).ReturnsAsync((Game)null);
+
+            var gamesApiController = new GamesAPI(unitOfWorkMock.Object, new GameHelper(new GameHistoryHelper()), new GameListHelper());
+
+            var result = await gamesApiController.GetGameDetailsById(Guid.NewGuid());
 
             Assert.IsInstanceOf(typeof(NotFoundResult), result.Result);
         }
